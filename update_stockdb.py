3a3dd5782c1c7c377aa463e07f6c68c937edcece
mm <mmm@zzz.com>
2020-07-15 01:30:01 +0900

    stooqからlocal mysqlへの保存ルーチンできた。すでにDB内に存在するデータも持ってきてしまうところが改善ポイント。

---
+++ update_stockdb.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# coding: utf-8
+#
+from urllib.parse import urlparse
+import mysql.connector
+import pandas_datareader.data as web
+import datetime
+
+# company codeのリストを返す
+def company_codes():
+    return ['6701.JP', '6702.JP']
+
+
+class stockdb():
+
+    #
+    def __init__(self):
+        url = urlparse('mysql://stockdb:bdkcots@192.168.1.11:3306/stockdb')
+        self.mydb = mysql.connector.connect(
+            host=url.hostname,
+            port=url.port,
+            user=url.username,
+            database=url.path[1:],
+            password=url.password
+        )
+        self.mycursor = self.mydb.cursor(buffered=True)
+        sql = 'CREATE TABLE IF NOT EXISTS stockdb ('
+        sql += 'date DATE NOT NULL, '
+        sql += 'cc VARCHAR(16) NOT NULL, '
+        sql += 'open FLOAT, close FLOAT, high FLOAT, low FLOAT, volume INT,'
+        sql += 'PRIMARY KEY(date, cc)'
+        sql += ')'
+        self.mycursor.execute(sql)
+
+    def __del__(self):
+        self.mydb.close()
+
+    def get_start_date(self, company_code):
+        sql = 'SELECT date from %s where cc = "%s" ORDER BY date DESC limit %d;' % ('stockdb', company_code, 1)
+        self.mycursor.execute(sql)
+        if self.mycursor.rowcount == 0:
+            return ""
+        else:
+            return self.mycursor.fetchone()[0]
+
+
+    #
+    def get_data_from_stooq(self, company_code, start_date):
+        if start_date == "":
+            start_date = datetime.datetime(2010, 1, 1)
+
+        # TODO: 開始日指定しているのに全データを取ってきている。
+        # tsd = web.DataReader("6702.JP", "stooq", start="2020/1/1", end="2020/5/5").dropna()
+        tsd = web.DataReader(company_code, "stooq", start_date.strftime('%Y/%m/%d')).dropna()
+        return tsd
+
+    def insert_data(self, company_code, data):
+        for date in data.index:
+            row = data.loc[date]
+            sql = 'INSERT INTO %s (%s, %s, %s, %s, %s, %s, %s) VALUES ("%s", "%s", %f, %f, %f, %f, %d)' % \
+                  ('stockdb',
+                   'date', 'cc', 'open', 'close', 'high', 'low', 'volume',
+                   date, company_code,
+                   row['Open'], row['Close'], row['High'], row['Low'], row['Volume']
+                   )
+            try:
+                self.mycursor.execute(sql)
+            except mysql.connector.IntegrityError as e:
+                print("history already exist: %s" % e)
+
+
+        self.mydb.commit()
+
+    # DBの株価を更新する
+    def update_stockdb(self, company_code):
+
+        ## DBにアクセスしてDB内の最新の日付をゲット
+        startdate = self.get_start_date(company_code)
+
+        ## stooqにアクセスして、株価を入手
+        stooq_data = self.get_data_from_stooq(company_code, startdate)
+
+        ## DBの更新
+        self.insert_data(company_code, stooq_data)
+
+
+
+if __name__ == "__main__":
+    stockdb = stockdb()
+    for cc in company_codes():
+        stockdb.update_stockdb(cc)
