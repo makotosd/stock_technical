462c5162a5a654c46f259c7f97981c5df9f41a6c
mm <mmm@zzz.com>
2020-08-10 23:16:19 +0900

    stockdbをpartition化。実際にはmysqlのalterコマンドを直接実行したので、このスクリプトは使っていないが、一応反映させておく。

--- update_stockdb.py
+++ update_stockdb.py
@@ -41,7 +41,7 @@ class stockdb():
         sql += 'datagettime DATETIME, '
         sql += 'valid boolean,'
         sql += 'PRIMARY KEY(cc)'
-        sql += ')'
+        sql += ') PARTITION BY KEY(cc) PARTITIONS 4096;'
         self.mycursor.execute(sql)
 
         # 一旦全部invalid
