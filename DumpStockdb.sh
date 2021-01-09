9f2a419168507c42cd04e17c72b2e3cb47836550
mm wsl <zzz@mmm.com>
2021-01-10 01:05:09 +0900

    dumpstockdb.sh runs on WSL client

--- DumpStockdb.sh
+++ DumpStockdb.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 #
 #
-mysqldump -u stockdb -p -h 192.168.1.11 stockdb data_j stockdb --single-transaction > stockdb.dump
-zip stockdb.zip stockdb.dump
+# mysqldump -u stockdb -p -h 192.168.1.11 stockdb data_j stockdb --single-transaction > stockdb.dump
+mysqldump --single-transaction -u stockdb -p stockdb --password=xxxxxxx > stockdb.dump
+zip /mnt/e/stockdb.zip stockdb.dump
 rm stockdb.dump
