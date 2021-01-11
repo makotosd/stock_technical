22bb8f8cc7f1b432fbb06e417fb75c641e3d7d8e
mm wsl <zzz@mmm.com>
2021-01-11 21:11:23 +0900

    dairy uption

--- UpdateStockdb.sh
+++ UpdateStockdb.sh
@@ -2,5 +2,5 @@
 #
 #
 # python3 update_stockdb.py > output.log 2>&1
-python3 update_stockdb.py --dropdb --historydb --update_by_nikkei > output.log 2>&1
-
+# python3 update_stockdb.py --dropdb --historydb --update_by_nikkei > output.log 2>&1
+python3 update_stockdb.py --url mysql+mysqlconnector://stockdb:bdkcots@localhost:3306/stockdb --update_by_nikkei --sleep 0.5 > output.log 2>&1
